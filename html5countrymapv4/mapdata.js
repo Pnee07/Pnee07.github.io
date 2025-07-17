c0aa03e8886bc7cbdcf70cde714a3125bdf4e858
Pnee07 <kyengrok11@gmail.com>
2025-07-17 15:04:40 +0900

    Add files via upload

---
+++ html5countrymapv4/mapdata.js
@@ -0,0 +1,344 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    state_description: "",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "<img src=\"https://simplemaps.com/static/img/frog.png\" />",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: "50",
+    location_type: "marker",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 16,
+    label_font: "Arial",
+    label_display: "auto",
+    label_scale: "yes",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    KR11: {
+      name: "서울"
+    },
+    KR26: {
+      name: "부산"
+    },
+    KR27: {
+      name: "대구"
+    },
+    KR28: {
+      name: "인천"
+    },
+    KR29: {
+      name: "광주"
+    },
+    KR30: {
+      name: "대전"
+    },
+    KR31: {
+      name: "울산"
+    },
+    KR41: {
+      name: "경기도"
+    },
+    KR42: {
+      name: "강원도"
+    },
+    KR43: {
+      name: "충청북도"
+    },
+    KR44: {
+      name: "충청남도"
+    },
+    KR45: {
+      name: "전라북도"
+    },
+    KR46: {
+      name: "전라남도"
+    },
+    KR47: {
+      name: "경상북도"
+    },
+    KR48: {
+      name: "경상남도"
+    },
+    KR49: {
+      name: "제주도"
+    },
+    KR50: {
+      name: "세종"
+    }
+  },
+  locations: {
+    "0": {
+      name: "서울특별시",
+      lat: "37.5985",
+      lng: "126.9783",
+      description: "<img src=\"https://i.namu.wiki/i/stSOmJ9FjloQMixj9M_0xfhUkTh5PtEQCUB_d1UV0LUflUAnSMLA3c0pou3PXsSut-witSK3LD9QgLaZSwLMlQ.webp\" style=\"width: 300px\" /><br> 7반 하준민 - 내가 갈 학교"
+    },
+    "1": {
+      lat: "33.2530",
+      lng: "126.5218",
+      name: "서귀포시",
+      description: "<img src=\"https://i.imgur.com/evERFE8.jpg\" style=\"width: 300px\" /><br> 2반 최세현 - 중앙고 2학년 수학여행",
+      popups: "https://simplemaps.com/docs/"
+    },
+    "2": {
+      lat: "35.8562",
+      lng: "129.2247",
+      name: "경주시",
+      description: "<img src=\"https://i.imgur.com/rO0bop3.jpeg\" style=\"width: 300px\" /><br> 2반 강다율 - 경주에 가족여행을 가서 다보탑을 봤다"
+    },
+    "3": {
+      description: "<img src=\"https://i.imgur.com/uLRQLYw.jpeg\" style=\"width: 160px\" /><img src=\"https://i.imgur.com/t3UnqH9.jpeg\" style=\"width: 160px\" /><br> 2반 최세현 - 인천 인스파이어아레나에서 본 콘서트",
+      name: "인천광역시",
+      lat: "37.4563",
+      lng: "126.7052"
+    },
+    "4": {
+      description: "<img src=\"https://i.imgur.com/9lKfK63.jpeg\" style=\"width: 300px\" /><br> 3반 최주원 - 진해인데 그냥 예뻐서 찍음",
+      name: "창원시 진해구",
+      lat: "35.1461",
+      lng: "128.6598"
+    },
+    "5": {
+      description: "<img src=\"https://i.imgur.com/iKDSFIg.jpeg\" style=\"width: 300px\" /><br> 7반 하준민 - 전북 무주 태권도원 구경",
+      lat: "35.9952",
+      lng: "127.6603",
+      name: "무주군"
+    },
+    "6": {
+      description: "<img src=\"https://i.imgur.com/UDaY2h0.jpeg\" style=\"width: 275px\" /><br> 4반 강민재 - 부산 블루라인파크",
+      name: "부산광역시",
+      lat: "35.1796",
+      lng: "129.0756"
+    },
+    "7": {
+      name: "아산시",
+      lat: "36.7920",
+      lng: "127.0017",
+      description: "<img src=\"https://i.imgur.com/13XDsnY.jpeg\" style=\"width: 320px\" /><br> 6반 하승원 - 여행 갔는데 눈보라에 길을 잃다"
+    },
+    "8": {
+      description: "<img src=\"https://i.imgur.com/sjdZEIC.jpeg\" style=\"width: 300px\" /><br> 2반 강다율 - 통영 여행",
+      name: "통영시",
+      lat: "34.8544",
+      lng: "128.4333"
+    },
+    "9": {
+      description: "<img src=\"https://i.imgur.com/dPI2cC1.jpeg\" style=\"width: 2700px\" /><br> 4반 강민재 - 대구, 청도, 창녕 사이 어딘가에 있는 비슬산",
+      name: "대구광역시",
+      lat: "35.7810",
+      lng: "128.5323"
+    },
+    "10": {
+      name: "전주시",
+      lat: "35.8154",
+      lng: "127.1527",
+      description: "<img src=\"https://mblogthumb-phinf.pstatic.net/MjAyMzAzMDZfNTEg/MDAxNjc4MTA1NTgxNjE5.juif4UWlIAvu4kC-BnChdHDpAfyAwjcly-tNtvI5_NMg.jaiUB1Bg0-nvlcv6qWf3DOlVohFRMqpCj4fBL5Ckdjog.JPEG.s600105/20230306＿181903.jpg?type=w800\" style=\"width: 300px\" /><br> 2반 손준후 - 전주 한옥마을"
+    },
+    "11": {
+      lat: "34.7382",
+      lng: "128.6303",
+      name: "거제시",
+      description: "<img src=\"https://i.imgur.com/jyjFVee.jpeg\" style=\"width: 300px\" /><br> 7반 김승준 - 배 타고 간 거제도"
+    },
+    "12": {
+      description: "<img src=\"https://cdn.gjdream.com/news/photo/202209/617782_218135_14.jpg\" style=\"width: 260px\" /><br> 3반 이현성 - 여수밤바다",
+      name: "여수시",
+      lat: "34.7366",
+      lng: "127.7416"
+    },
+    "13": {
+      name: "▶중앙고 수학여행 브이로그",
+      description: "<img src=\"https://i.imgur.com/diQUTMq.jpeg\" style=\"width: 200px\" /><br>(클릭해서 확인)",
+      url: "https://www.youtube.com/watch?v=WbRXjUqkMw4",
+      lat: "33.3200",
+      lng: "126.6500",
+      type: "heart",
+      size: "32",
+      color: "#88FF00"
+    },
+    "14": {
+      name: "보성군",
+      lat: "34.7748",
+      lng: "127.0801",
+      description: "<img src=\"https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/de04eee4-e246-46e5-9d87-98562cfd5cf8.jpeg\" style=\"width: 260px\" /><br>2반 설성윤 - 녹차 먹으러 왔습니다"
+    },
+    "15": {
+      url: "https://www.youtube.com/watch?v=SwP10CCP4lc",
+      description: "(클릭해서 확인)",
+      lat: "33.3030",
+      lng: "129.4047",
+      type: "heart",
+      size: "32",
+      name: "▶한국 주요 지역들 4분만에 외우기!",
+      color: "#88FF00"
+    },
+    "16": {
+      description: "<img src=\"https://i.imgur.com/iUIsomS.png\" style=\"width: 300px\" /><br>3반 조성준, 4반 구동하 - 그날 우리는 우리의 미래를 보았다. 로블록스 강원도 체험에서.",
+      name: "강릉시",
+      lat: "37.7519",
+      lng: "128.8761"
+    },
+    "17": {
+      description: "<img src=\"https://i.imgur.com/cQeW2Ls.jpeg\" style=\"width: 250px\" /><br>3반 조성준 - 티익스프레스 저리가라",
+      name: "충주시",
+      lat: "36.9910",
+      lng: "127.9251"
+    },
+    "18": {
+      lat: "37.3815",
+      lng: "126.6456",
+      name: "인천광역시",
+      description: "<img src=\"https://i.imgur.com/Y5C20PR.jpeg\" style=\"width: 300px\" /><br>4반 강민재 - 내가 갈 학교 (연세대 송도 국제캠)"
+    },
+    "19": {
+      name: "안동시",
+      description: "<img src=\"https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDExMTlfMjgg%2FMDAxNzMxOTg2OTAxMDM2.M7we08HwO8j6VrHCGpeT9rmWW_t8r9J_7vB-GQg3Gegg.X61K3PKgWuXIQeIB2yROSTbUBYVA-iuKL-NOtFbIrsYg.JPEG%2FIMG_4831.jpg&type=sc960_832\" style=\"width: 280px\" /><br>7반 김태윤 - 안동 하회마을에 왔다. 조선시대의 마을이 세워져있었다. 내가 만약 이 시대에 태어났다면 나는 천생 노비 출신이었을 것 같다.",
+      lat: "36.5383",
+      lng: "128.5184"
+    },
+    "20": {
+      description: "<img src=\"https://i.imgur.com/zk65Iqs.jpeg\" style=\"width: 260px\" /><br>2반 정아라한 - 부엉이와 함께 밥을",
+      name: "대전광역시",
+      lat: "36.3504",
+      lng: "127.3845"
+    },
+    "21": {
+      description: "<img src=\"https://i.imgur.com/A56S8b4.png\" style=\"width: 300px\" /><br>2반 정범준 - 속초 설악산",
+      lat: "38.1641",
+      lng: "128.5241",
+      name: "속초시"
+    }
+  },
+  labels: {
+    KR11: {
+      name: "Seoul",
+      parent_id: "KR11"
+    },
+    KR26: {
+      name: "Busan",
+      parent_id: "KR26"
+    },
+    KR27: {
+      name: "Daegu",
+      parent_id: "KR27"
+    },
+    KR28: {
+      name: "Incheon",
+      parent_id: "KR28"
+    },
+    KR29: {
+      name: "Gwangju",
+      parent_id: "KR29"
+    },
+    KR30: {
+      name: "Daejeon",
+      parent_id: "KR30"
+    },
+    KR31: {
+      name: "Ulsan",
+      parent_id: "KR31"
+    },
+    KR41: {
+      name: "Gyeonggi",
+      parent_id: "KR41"
+    },
+    KR42: {
+      name: "Gangwon",
+      parent_id: "KR42"
+    },
+    KR43: {
+      name: "North Chungcheong",
+      parent_id: "KR43"
+    },
+    KR44: {
+      name: "South Chungcheong",
+      parent_id: "KR44"
+    },
+    KR45: {
+      name: "North Jeolla",
+      parent_id: "KR45"
+    },
+    KR46: {
+      name: "South Jeolla",
+      parent_id: "KR46"
+    },
+    KR47: {
+      name: "North Gyeongsang",
+      parent_id: "KR47"
+    },
+    KR48: {
+      name: "South Gyeongsang",
+      parent_id: "KR48"
+    },
+    KR49: {
+      name: "Jeju",
+      parent_id: "KR49"
+    },
+    KR50: {
+      name: "Sejong",
+      parent_id: "KR50"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
